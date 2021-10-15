2e40e5589aa2d3e3d8a685edec572d4989b61f4a
Victor Aguiar <victor_a.ribeiro@outlook.com>
2021-10-15 19:59:21 -0300

    Adicionando o projeto MusicDot (quase) completo - Baseado na apostila de Desenvolvimento Web da Caelum, disponível no link: https://www.caelum.com.br/apostila-html-css-javascript

---
+++ js/matricula.js
@@ -0,0 +1,34 @@
+﻿var $tdTotalCursos = document.querySelector('.js-total-de-cursos')
+var $tdTotalDeHoras = document.querySelector('.js-total-de-horas')
+var $btnConfirmar = document.querySelector('.js-botao-matricula')
+
+var totalHoras = 0
+var totalCursos = 0
+
+function addCurso(checkbox) {
+
+    if (checkbox.checked) {
+        totalCursos ++
+        totalHoras += parseInt(checkbox.value)
+    }
+    else {
+        totalCursos --
+        totalHoras -= parseInt(checkbox.value)
+    }
+
+    $tdTotalDeHoras.textContent = totalHoras + 'h'
+    $tdTotalCursos.textContent = totalCursos + " curso(s)"
+
+}
+
+$btnConfirmar.onclick = confirmaMatriculas
+
+function confirmaMatriculas() {
+    if(totalCursos == 0) {
+        alert('Nenhum curso selecionado')
+    } else {
+        alert('Matrícula confirmada nos cursos!')
+        window.location.href = 'index.html'
+    }
+}
+
